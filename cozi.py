f867109aec1a5d4253d3ff30fbf8798f3b06dd40
Kat Schlorff <kat.schlorff@proton.me>
2024-06-15 14:01:58 -0500

    sort results from soonest to latest, some local testing printing

--- cozi.py
+++ cozi.py
@@ -45,20 +45,23 @@ def get_the_state_of_the_world_from_cozi() -> [[], []]:
     #     print(fe.begin.humanize())
 
 
-@cozi_bot_client.event
+# @cozi_bot_client.event
 async def on_ready():  # Called when internal cache is loaded
     all_events = get_the_state_of_the_world_from_cozi()
     future_events = all_events[1]
     channel = discord.utils.get(cozi_bot_client.get_all_channels(),
                                 name=channel_name)  # Gets channel from internal cache
     await channel.send('Upcoming events!\n')
-    for event in future_events:
-        await channel.send(event.name)
-        await channel.send(event.begin.humanize())
+    for event in sorted(future_events):
+        print(event.name)
+        print(event.begin.humanize())
+        # await channel.send(event.name)
+        # await channel.send(event.begin.humanize())
 
 
 if __name__ == '__main__':
-    cozi_bot_client.run(token=discord_token)
-    cozi_bot_client.close()
+    pass
+    # cozi_bot_client.run(token=discord_token)
+    # cozi_bot_client.close()
     # # get_the_state_of_the_world_from_cozi()
     # channel.send('hello world!')
